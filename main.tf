0db67bae11b90b5222776d222e331ff194fbe256
haseeb <mohammedhaseeb47@gmail.com>
2024-08-06 18:47:29 +0530

    first commit on terraform

---
+++ main.tf
@@ -0,0 +1,4 @@
+resource "azurerm_resource_group" "rg001" {
+  name = var.rgname
+  location = var.location
+}
---
+++ variable.tf
@@ -0,0 +1,7 @@
+variable "rgname" {
+    description = "name of the resource group"
+}
+
+variable "location" {
+  description = "location - westus, westeurope, etc,."
+}
